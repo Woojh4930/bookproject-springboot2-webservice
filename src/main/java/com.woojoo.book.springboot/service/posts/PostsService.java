65019aeaa72cede5dfceceeea5a8aff87ecdb854
juhwang <juhong4930@naver.com>
2023-01-10 17:23:47 +0900

    mustache를 이용한 게시글 등록 페이지 테스트

--- src/main/java/com.woojoo.book.springboot/service/posts/PostsService.java
+++ src/main/java/com.woojoo.book.springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.woojoo.book.springboot.service.posts;
 
 import com.woojoo.book.springboot.domain.posts.Posts;
 import com.woojoo.book.springboot.domain.posts.PostsRepository;
+import com.woojoo.book.springboot.web.dto.PostsListResponseDto;
 import com.woojoo.book.springboot.web.dto.PostsResponseDto;
 import com.woojoo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.woojoo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
