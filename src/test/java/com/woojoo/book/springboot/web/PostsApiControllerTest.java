53fbc334544d2af7e7340bcce02dd4a92f56b903
juhwang <juhong4930@naver.com>
2023-01-11 12:42:48 +0900

    게시글 삭제 테스트

--- src/test/java/com/woojoo/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/woojoo/book/springboot/web/PostsApiControllerTest.java
@@ -101,4 +101,35 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
+
+    @Test
+    public void Posts_삭제된다() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long deleteId = savedPosts.getId();
+        int expectSize = 0;
+
+        Posts posts = postsRepository.findById(deleteId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다.id=" + deleteId));
+
+        String url = "http://localhost:" +
+                port +
+                "/api/v1/posts/" +
+                deleteId;
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url,HttpMethod.DELETE,HttpEntity.EMPTY, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.size()).isEqualTo(expectSize);
+    }
 }
