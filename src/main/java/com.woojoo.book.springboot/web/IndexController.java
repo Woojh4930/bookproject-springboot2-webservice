08dfa28564ae15a5191801ff638c616dfd85cbbe
juhwang <juhong4930@naver.com>
2023-01-11 16:13:32 +0900

    로그인 어노테이션 적용

--- src/main/java/com.woojoo.book.springboot/web/IndexController.java
+++ src/main/java/com.woojoo.book.springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.woojoo.book.springboot.web;
 
+import com.woojoo.book.springboot.config.auth.LoginUser;
 import com.woojoo.book.springboot.config.auth.dto.SessionUser;
 import com.woojoo.book.springboot.service.posts.PostsService;
 import com.woojoo.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
